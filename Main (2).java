2f7ddb1cd32fc2901ab0d1c59c3f0c02bd254fd2
Bhanu Reddy <144329092+BhanuReddy421@users.noreply.github.com>
2025-06-17 14:17:45 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n<0){
+System.out.println("negative");
+}
+else if(n>0){
+System.out.println("positive");
+}
+else{
+System.out.println("zero");
+}
+}
+}
---
+++ Main2.java
@@ -0,0 +1,12 @@
+import java.util.Stack;   
+public class Main2
+{
+	public static void main(String[] args) {
+		Stack s=new Stack<>();
+		s.push(1);
+		s.push(2);
+		System.out.println(s.peek());
+		System.out.println(s.pop());
+		
+	}
+}
